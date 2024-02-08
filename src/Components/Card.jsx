3fe99e24ca684b7ba1d4f8b5b16e17b139a8c4b2
Alec Hannaford <alec322@gmail.com>
2024-02-07 16:41:48 -0800

    icons added to card for meat and allergy

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,19 +8,44 @@ function Card(props){
         >
             <h3>{props.sausage.name}</h3>
             <ul className="allergies">
-                {props.sausage.isLactoseFree && <li>lactose free</li>}
-                {props.sausage.isNutFree && <li>nut free</li>}
-                {props.sausage.isGlutenFree && <li>Gluten free</li>}
-                {props.sausage.isSugarFreee && <li>sugar free</li>}
-                {props.sausage.isEggFree && <li>egg free</li>}
+                {props.sausage.isLactoseFree && <li className="icon">
+                <img
+                src="https://imgur.com/tqpk8Vq.png"
+                />    
+                </li>}
+                {props.sausage.isNutFree && <li className="icon">
+                <img
+                src="https://imgur.com/OVuWr7k.png"
+                />    
+                </li>}
+                {props.sausage.isGlutenFree && <li className="icon">
+                <img
+                src="https://imgur.com/leN7joT.png"
+                />    
+                </li>}
+                {props.sausage.isSugarFreee && <li className="icon">
+                <img
+                src="https://imgur.com/nCxVwJN.png"
+                />    
+                </li>}
+                {props.sausage.isEggFree && <li className="icon">
+                <img
+                src="https://imgur.com/JUKRCq4.png"
+                />    
+                </li>}
             </ul>
             <div className="spice">
                 <SpiceMeter spiceLevel={props.sausage.spiceLevel}/>
             </div>
             <ul className="animals">
-                {props.sausage.isBeef && <li>beef</li>}
-                {props.sausage.isPork && <li>pork</li>}
-                {props.sausage.isChicken && <li>chicken</li>}
+                {props.sausage.isBeef && <li className="icon"><img
+                src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Silhouettes-PNG/Cow_Silhouette_PNG_Clip_Art_Image.png?m=1510113302"/></li>}
+                {props.sausage.isPork && <li className="icon"><img
+                src="https://i.pinimg.com/originals/ea/89/94/ea8994889ef97021a1b6b4feece51d9c.png"
+                /></li>}
+                {props.sausage.isChicken && <li className="icon"><img
+                src="https://img2.embroiderydesigns.com/printart/xlarge/cosmos_finearts/pgcfa1504.webp"
+                /></li>}
             </ul>
         </div>
         <div
